da901704515cd239486858795e9e9d75c4b1bbbb
trieutuhuy <108987410+trieutuhuy@users.noreply.github.com>
2023-03-01 19:30:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var imgWidth = 120; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
+var bgMusicURL = 'https://soundcloud.com/bet-music-nation-465634881';
 var bgMusicControls = true; // Show UI music control
 
 
